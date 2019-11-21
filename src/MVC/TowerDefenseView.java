742591f042175f734f4c3325b406c1696baf9907
jason <jason@dhcp-10-142-210-74.uawifi.arizona.edu>
2019-11-21 16:45:54 -0700

    Top container for view with buttons

--- src/MVC/TowerDefenseView.java
+++ src/MVC/TowerDefenseView.java
@@ -4,27 +4,61 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TowerDefenseView extends Application implements Observer{
 
-	
+	public TowerDefenseController tower;
 	
 	public TowerDefenseView() {
-		System.out.println("hello");
+		TowerDefenseModel model = new TowerDefenseModel();
+		model.addObserver(this);
+		tower = new TowerDefenseController();
 	}
 	
 	
 	
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Tower Defense");
+		BorderPane window = new BorderPane();
+		Scene scene = new Scene(window, 1000, 1000);
+		createTopContainer(window);
+		//createMainContainer(window);
+		//createRightContainer(window);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 	
 	
+	private void createTopContainer(BorderPane window) {
+		HBox hBox = new HBox();
+		Button newGame = new Button("New Game");
+		Button pause = new Button("Pause");
+		Button speedUp = new Button("Speed++");
+		hBox.getChildren().add(newGame);
+		hBox.getChildren().add(pause);
+		hBox.getChildren().add(speedUp);
+		window.setRight(hBox);
+		
+	}
+	
+	
+	private void createMainContainer(BorderPane window) {
+		VBox vBox = new VBox();
+	}
+	
+	
+	private void createRightContainer(BorderPane window) {
+		
+	}
 	
 	
 	
