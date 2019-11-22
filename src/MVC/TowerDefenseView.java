94f01691bdad6f99cd325b49f921715122cad373
jason <jason@dhcp-10-142-210-74.uawifi.arizona.edu>
2019-11-21 18:26:54 -0700

    GUI top menu create with event handlers. Right container partially finished.

--- src/MVC/TowerDefenseView.java
+++ src/MVC/TowerDefenseView.java
@@ -5,20 +5,24 @@ import java.util.Observer;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class TowerDefenseView extends Application implements Observer{
 
-	public TowerDefenseController tower;
+	public TowerDefenseController controller;
 	
 	public TowerDefenseView() {
 		TowerDefenseModel model = new TowerDefenseModel();
 		model.addObserver(this);
-		tower = new TowerDefenseController();
+		controller = new TowerDefenseController();
 	}
 	
 	
@@ -30,32 +34,80 @@ public class TowerDefenseView extends Application implements Observer{
 		Scene scene = new Scene(window, 1000, 1000);
 		createTopContainer(window);
 		//createMainContainer(window);
-		//createRightContainer(window);
+		createRightContainer(window);
 		stage.setScene(scene);
 		stage.show();
 	}
 	
 	
-	
+	/**
+	 * Top container has the menu drop down. Pause and speed up are disabled
+	 * until new game is clicked.
+	 * @param window sets the menu bar in window.
+	 */
 	private void createTopContainer(BorderPane window) {
-		HBox hBox = new HBox();
-		Button newGame = new Button("New Game");
-		Button pause = new Button("Pause");
-		Button speedUp = new Button("Speed++");
-		hBox.getChildren().add(newGame);
-		hBox.getChildren().add(pause);
-		hBox.getChildren().add(speedUp);
-		window.setRight(hBox);
+		MenuBar menuBar = new MenuBar();
+		Menu menu = new Menu("Menu");
+		MenuItem newGame = new MenuItem("New Game");
+		MenuItem pause = new MenuItem("Pause");
+		MenuItem speedUp = new MenuItem("Speed++");
+		pause.setDisable(true); //disable until new game is clicked
+		speedUp.setDisable(true); // disable until new game is clicked
 		
+		//event handler
+		newGame.setOnAction(event->{
+			startNewGame(pause, speedUp); //parameters used when new game
+		});
+		
+		
+		menu.getItems().addAll(newGame, pause, speedUp);
+		menuBar.getMenus().add(menu);
+		VBox menuBox = new VBox(menuBar);
+		window.setTop(menuBox);
 	}
 	
 	
 	private void createMainContainer(BorderPane window) {
-		VBox vBox = new VBox();
+		
 	}
 	
 	
 	private void createRightContainer(BorderPane window) {
+		VBox vBox = new VBox();
+		HBox moneyBox = new HBox();
+		TextFlow moneyDisplay = new TextFlow();
+		Text moneyTitle = new Text("Balance: ");
+		
+		
+		//controller.getStartingBalance 
+		Text moneyBalance = new Text("1000");
+	
+		moneyBox.getChildren().addAll(moneyTitle, moneyBalance);
+		
+	
+	
+		vBox.getChildren().add(moneyBox);
+		window.setRight(vBox);
+	}
+	
+	
+	//Event handlers methods
+	private void startNewGame(MenuItem pause, MenuItem speedUp) {
+		pause.setOnAction(event->{
+			pauseGame();
+		});
+		speedUp.setOnAction(event->{
+			speedUpGame();
+		});
+		
+		
+	}
+	
+	private void pauseGame() {
+		
+	}
+	
+	private void speedUpGame() {
 		
 	}
 	
